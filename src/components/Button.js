44cbc50cd343d069bc26301650fe725b923f08e8
Luis Zubia <soyzubia@gmail.com>
2023-03-21 11:07:30 -0700

    Add optional class to Button component, so that we can style it according to the design

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,14 @@
 import PropTypes from 'proptypes';
 
-const Button = ({ name }) => (
-  <button type="button">
+const Button = ({ name, classType }) => (
+  <button type="button" className={classType}>
     {name}
   </button>
 );
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  classType: PropTypes.string.isRequired,
 };
 
 export default Button;
