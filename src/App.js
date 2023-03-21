8d8fa4addce4af314e7bab6b2d2098c8b307ea78
Luis Zubia <soyzubia@gmail.com>
2023-03-21 11:09:46 -0700

    Main App component now renders a welcome message and the Calculator component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Calculator from './components/Calculator';
 import './assets/styles/App.css';
 
 const App = () => (
   <>
     <h1>Welcome to Math Magicians</h1>
+    <Calculator />
   </>
 );
 
