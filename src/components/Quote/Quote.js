157d9e256e1d5d24b0b816a974da5592a41df82f
Luis Zubia <soyzubia@gmail.com>
2023-03-22 21:28:02 -0700

    Quote component now includes all the fetch logic to retrieve a quote from the API

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,20 +1,75 @@
-import PropTypes from 'proptypes';
+import { useEffect, useState } from 'react';
+import { API_URL, HEADERS_LIST } from '../../api/apiSettings';
 
-const Quote = ({ quote, author }) => (
-  <>
-    <h2>Today&apos;s Quote</h2>
-    <q>
-      {quote}
-    </q>
-    <p>
-      {author}
-    </p>
-  </>
-);
+const Quote = () => {
+  const [dailyQuote, setDailyQuote] = useState({
+    quote: null, author: null, isLoading: false, hasError: false,
+  });
 
-Quote.propTypes = {
-  quote: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
+  useEffect(() => {
+    const fetchQuote = async (quoteType = 'inspirational') => {
+      setDailyQuote((prev) => ({
+        ...prev,
+        isLoading: true,
+        hasError: false,
+      }));
+
+      const fetchUrl = `${API_URL}${quoteType}`;
+      const request = new Request(
+        fetchUrl, {
+          method: 'GET',
+          'Content-Type': 'application/json',
+          headers: HEADERS_LIST,
+        },
+      );
+
+      try {
+        const response = await fetch(request);
+        const [json] = await response.json();
+        const { quote, author } = json;
+        setDailyQuote((prev) => ({
+          ...prev,
+          quote,
+          author,
+          hasError: false,
+        }));
+      } catch (error) {
+        setDailyQuote((prev) => ({
+          ...prev,
+          hasError: true,
+        }));
+      }
+
+      setDailyQuote((prev) => ({
+        ...prev,
+        isLoading: false,
+      }));
+    };
+
+    fetchQuote();
+  }, []);
+
+  const {
+    isLoading, hasError, quote, author,
+  } = dailyQuote;
+
+  return (
+    <div className="quote">
+      <h2>Today&apos;s Quote</h2>
+      {isLoading && <p>Daily Quote is Loading...</p>}
+      {hasError && <p>An Error ocurrs while loading the Daily Quote.</p>}
+      {!isLoading && !hasError && (
+      <>
+        <q>
+          {quote}
+        </q>
+        <p>
+          {author}
+        </p>
+      </>
+      ) }
+    </div>
+  );
 };
 
 export default Quote;
