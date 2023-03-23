2590551631bac223ed90d43a3bfc1a3f1617523c
Luis Zubia <soyzubia@gmail.com>
2023-03-22 21:26:15 -0700

    Move all fetch from API logic to Quote component

--- src/App.js
+++ src/App.js
@@ -1,61 +1,13 @@
-import { useEffect, useState } from 'react';
-import { API_URL, HEADERS_LIST } from './api/apiSettings';
 import Calculator from './components/Calculator/Calculator';
 import Quote from './components/Quote/Quote';
 import './assets/styles/App.css';
 
-const App = () => {
-  const [dailyQuote, setDailyQuote] = useState({ quote: '', author: '' });
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasError, setHasError] = useState(false);
-
-  useEffect(() => {
-    const fetchQuote = async (quoteType = 'inspirational') => {
-      setIsLoading(true);
-
-      const fetchUrl = `${API_URL}${quoteType}`;
-      const request = new Request(
-        fetchUrl, {
-          method: 'GET',
-          'Content-Type': 'application/json',
-          headers: HEADERS_LIST,
-        },
-      );
-
-      try {
-        const response = await fetch(request);
-        const [json] = await response.json();
-        setDailyQuote((prev) => ({
-          ...prev,
-          ...json,
-        }));
-
-        setHasError(false);
-      } catch (error) {
-        setHasError(true);
-      }
-
-      setIsLoading(false);
-    };
-
-    fetchQuote();
-  }, []);
-
-  return (
-    <>
-      <h1>Welcome to Math Magicians</h1>
-      <Calculator />
-      { isLoading
-        ? (<p>Daily Quote is Loading...</p>)
-        : (
-          <Quote
-            quote={dailyQuote.quote}
-            author={dailyQuote.author}
-          />
-        ) }
-      { hasError && <p>Something went wrong while fetching the daily quote.</p> }
-    </>
-  );
-};
+const App = () => (
+  <>
+    <h1>Welcome to Math Magicians</h1>
+    <Calculator />
+    <Quote />
+  </>
+);
 
 export default App;
