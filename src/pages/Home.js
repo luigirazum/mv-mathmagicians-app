60d79ec44e0596fc30e4bb954634632b7603b442
Luis Zubia <soyzubia@gmail.com>
2023-03-28 21:34:20 -0700

    Add lorem ipsum content and Welcome message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,26 @@
 const Home = () => (
-  <h1>Home Page</h1>
+  <>
+    <h1>Hello there, Welcome!</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed ut labore et dolore magna aliqua.
+      Fermentum sed. Suspendisse faucibus interdum posuere lorem. Vulputate mi sit amet mauris.
+      Diam phasellus vestibulum lorem sed. Id neque aliquam vestibulum morbi blandit cursus.
+      Facilisis mauris sit amet massa. A pellentesque sit amet porttitor eget dolor morbi non arcu.
+      Iaculis urna id volutpat lacus. Nulla facilisi morbi tempus iaculis urna id volutpat.
+      Lectus urna duis convallis convallis tellus id interdum velit.
+      Urna id volutpat lacus laoreet non curabitur gravida arcu.
+    </p>
+    <p>
+      Cursus mattis molestie a iaculis at erat pellentesque. Ac tortor vitae purus faucibus ornare.
+      Tempus imperdiet nulla malesuada pellentesque elit eget  ut ornare lectus sit amet.
+      Augue ut lectus vel pharetra vel. Ac tortor dignissim convallis aenean et tortor at risus.
+      Ac tortor vitae purus faucibus ornare. Sed vulputate mi sit amet. Duis at faucibus molestie.
+      Tincidunt vitae semper quis lectus nulla. Ultricies tristique nulla aliquet enim tortor at.
+      Viverra justo nec ultrices dui sapien eget mi proin sed.Cursus aliquam sollicitudin aliquam.
+      Vitae curabitur vitae nunc sed. Interdum posuere lorem ipsum dolor. Ac felis donec et odio.
+    </p>
+
+  </>
 );
 
 export default Home;
