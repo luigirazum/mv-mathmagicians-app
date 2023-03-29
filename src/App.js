c6ea2172c62f71ba9648ef8561709189261f0ebd
Luis Zubia <soyzubia@gmail.com>
2023-03-28 21:32:01 -0700

    Fix typo on calculator and quote paths

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ const App = () => (
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/calculator" element={<MagicCalculator />} />
-        <Route path="/quote" element={<DailyQuote />} />
+        <Route path="calculator" element={<MagicCalculator />} />
+        <Route path="quote" element={<DailyQuote />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
