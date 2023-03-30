589cf5c0379247eda3dd039df581be5548d267ee
Luis Zubia <soyzubia@gmail.com>
2023-03-29 17:26:36 -0700

    Add content to Calculator page

--- src/pages/MagicCalculator.js
+++ src/pages/MagicCalculator.js
@@ -1,5 +1,13 @@
+import Calculator from '../components/Calculator/Calculator';
+
 const MagicCalculator = () => (
-  <h2>Here you see the Calculator</h2>
+  <div className="magicCalculator">
+    <div className="calcInfo">
+      <h2>Let&apos;s do some math!</h2>
+      <p>This is a fully working calculator. You can use it to do your homework.</p>
+    </div>
+    <Calculator />
+  </div>
 );
 
 export default MagicCalculator;
