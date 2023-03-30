50ac94876370af88287f72ca5baa6e0ec699e1bc
Luis Zubia <soyzubia@gmail.com>
2023-03-29 18:10:44 -0700

    Add intro content for the Calculator and Quote pages

--- src/pages/DailyQuote.js
+++ src/pages/DailyQuote.js
@@ -3,6 +3,17 @@ import Quote from '../components/Quote/Quote';
 const DailyQuote = () => (
   <>
     <h2>Today&apos;s Quote</h2>
+    <p>
+      Discover daily inspiration from some of the greatest minds in history.
+      Our carefully curated collection of quotes includes contributions from
+      mathematicians, scientists, philosophers, and more.
+    </p>
+    <p>
+      Each quote will challenge you to think differently about the world
+      around you and appreciate the power of human thought.
+      So go ahead and let the wisdom of the great minds of the past guide
+      you on your own journey.
+    </p>
     <Quote />
   </>
 );
--- src/pages/MagicCalculator.js
+++ src/pages/MagicCalculator.js
@@ -4,7 +4,21 @@ const MagicCalculator = () => (
   <div className="magicCalculator">
     <div className="calcInfo">
       <h2>Let&apos;s do some math!</h2>
-      <p>This is a fully working calculator. You can use it to do your homework.</p>
+      <p>
+        Here at Math Magicians, we believe that everyone should have access to a reliable and
+        easy-to-use calculator. That&apos;s why we&apos;ve embedded a basic calculator on this
+        page that you can use to perform simple or complex calculations, no matter where you are.
+      </p>
+      <p>
+        Our calculator is user-friendly and intuitive, making it perfect for students, teachers,
+        and anyone who needs to crunch numbers on a regular basis. With its simple interface
+        and versatile functionality, you can use our calculator for a wide range of
+        applications.
+      </p>
+      <p>
+        So go ahead and give our calculator a try! We&apos;re confident that you&apos;ll find it
+        to be a valuable tool in your mathematical arsenal.
+      </p>
     </div>
     <Calculator />
   </div>
